3232cf08dd654129673057cd29e9bad26886e5e5
vngarg <sngargrsd@gmail.com>
2020-03-04 17:39:25 +0530

    Updated

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -8,7 +8,7 @@ const UserItem= ({user:{login,avatar_url,html_url}}) => {
                 <img src={avatar_url} className="round-img" alt="...." style={{ width:'60px' , height:'60px'}}></img>
                 <h2>{login}</h2>
                 <div>
-                    <Link to={`/user/${login}`} className="btn btn-sm btn-dark" target="blank">More</Link>
+                    <Link to={`/user/${login}`} className="btn btn-sm btn-dark">More</Link>
                 </div>
             </div>
         )
